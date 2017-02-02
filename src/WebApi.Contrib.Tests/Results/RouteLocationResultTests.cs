d490678f29a266cb82348cfa34c37e08f458e106
Fabian Vilers <fabian.vilers@dev-one.com>
2017-02-02 10:25:28 +0100

    Add a base route location result

---
+++ src/WebApi.Contrib.Tests/Results/RouteLocationResultTests.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Routing;
+using WebApi.Contrib.Results;
+
+namespace WebApi.Contrib.Tests.Results
+{
+    [TestClass]
+    public class RouteLocationResultTests
+    {
+        private const string DefaultRouteName = "routeName";
+        private static readonly Uri DefaultLocation = new Uri("http://localhost/");
+
+        [TestMethod]
+        public async Task ExecuteAsyncShouldReturnAResponseWithTheSpecifiedStatusCode()
+        {
+            // Arrange
+            const HttpStatusCode statusCode = HttpStatusCode.TemporaryRedirect;
+            var sut = MakeSut(statusCode);
+
+            // Act
+            var result = await sut.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(statusCode, result.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsyncShouldReturnAResponseWithTheSpecifiedRequest()
+        {
+            // Arrange
+            var request = MakeRequest();
+            var sut = MakeSut(request: request);
+
+            // Act
+            var result = await sut.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(request, result.RequestMessage);
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsyncShouldReturnAResponseWithTheSpecifiedControllerRequest()
+        {
+            // Arrange
+            var request = MakeRequest();
+            var sut = MakeSutWithController(controller: MakeController(request));
+
+            // Act
+            var result = await sut.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(request, result.RequestMessage);
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public async Task ExecuteAsyncShouldThrowAnExceptionForInvalidLocationFormat()
+        {
+            // Arrange
+            var urlFactory = new Mock<UrlHelper>(MockBehavior.Strict);
+            urlFactory
+                .Setup(mock => mock.Link(It.IsAny<string>(), It.IsAny<IDictionary<string, object>>()))
+                .Returns<string>(null);
+            var sut = MakeSut(urlFactory: urlFactory.Object);
+
+            // Act
+            await sut.ExecuteAsync(CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsyncShouldReturnAResponseWithTheGeneratedLocation()
+        {
+            // Arrange
+            var urlFactory = new Mock<UrlHelper>(MockBehavior.Strict);
+            urlFactory
+                .Setup(mock => mock.Link(It.IsAny<string>(), It.IsAny<IDictionary<string, object>>()))
+                .Returns(DefaultLocation.ToString());
+            var sut = MakeSut(urlFactory: urlFactory.Object);
+
+            // Act
+            var result = await sut.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(DefaultLocation, result.Headers.Location);
+        }
+
+        private static HttpRequestMessage MakeRequest()
+        {
+            var request = new HttpRequestMessage();
+
+            return request;
+        }
+
+        private static ApiController MakeController(HttpRequestMessage request = null)
+        {
+            var controller = new TestController
+            {
+                ControllerContext = new HttpControllerContext
+                (
+                    new HttpConfiguration(),
+                    new HttpRouteData(new HttpRoute()),
+                    request ?? MakeRequest()
+                ),
+                Url = MakeUrlFactory()
+            };
+
+            return controller;
+        }
+
+        private static IHttpActionResult MakeSutWithController(HttpStatusCode? statusCode = null, string routeName = null, IDictionary<string, object> routeValues = null, ApiController controller = null)
+        {
+            var sut = new TestResult(
+                statusCode ?? HttpStatusCode.Ambiguous,
+                routeName ?? DefaultRouteName,
+                routeValues ?? MakeRouteValues(),
+                controller ?? MakeController()
+            );
+
+            return sut;
+        }
+
+        private static UrlHelper MakeUrlFactory()
+        {
+            var urlFactory = new Mock<UrlHelper>(MockBehavior.Strict);
+            urlFactory
+                .Setup(mock => mock.Link(It.IsAny<string>(), It.IsAny<IDictionary<string, object>>()))
+                .Returns(DefaultLocation.ToString());
+
+            return urlFactory.Object;
+        }
+
+        private static IDictionary<string, object> MakeRouteValues()
+        {
+            var routeValues = new Dictionary<string, object>();
+
+            return routeValues;
+        }
+
+        private static IHttpActionResult MakeSut(HttpStatusCode? statusCode = null, string routeName = null, IDictionary<string, object> routeValues = null, UrlHelper urlFactory = null, HttpRequestMessage request = null)
+        {
+            var sut = new TestResult(
+                statusCode ?? HttpStatusCode.Ambiguous,
+                routeName ?? DefaultRouteName,
+                routeValues ?? MakeRouteValues(),
+                urlFactory ?? MakeUrlFactory(),
+                request ?? MakeRequest()
+            );
+
+            return sut;
+        }
+
+        private class TestController : ApiController
+        {
+        }
+
+        private class TestResult : RouteLocationResult
+        {
+            public TestResult(HttpStatusCode statusCode, string routeName, IDictionary<string, object> routeValues, ApiController controller)
+                : base(statusCode, routeName, routeValues, controller)
+            {
+            }
+
+            public TestResult(HttpStatusCode statusCode, string routeName, IDictionary<string, object> routeValues, UrlHelper urlFactory, HttpRequestMessage request)
+                : base(statusCode, routeName, routeValues, urlFactory, request)
+            {
+            }
+        }
+    }
+}
---
+++ src/WebApi.Contrib/Results/RouteLocationResult.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace WebApi.Contrib.Results
+{
+    public abstract class RouteLocationResult : IHttpActionResult
+    {
+        public HttpStatusCode StatusCode { get; }
+        public string RouteName { get; }
+        public IDictionary<string, object> RouteValues { get; }
+
+        private readonly UrlHelper _urlFactory;
+        private readonly HttpRequestMessage _request;
+
+        protected RouteLocationResult(HttpStatusCode statusCode, string routeName, IDictionary<string, object> routeValues, ApiController controller)
+            : this(statusCode, routeName, routeValues, controller.Url, controller.Request)
+        {
+        }
+
+        protected RouteLocationResult(HttpStatusCode statusCode, string routeName, IDictionary<string, object> routeValues, UrlHelper urlFactory, HttpRequestMessage request)
+        {
+            if (!Enum.IsDefined(typeof(HttpStatusCode), statusCode)) throw new InvalidEnumArgumentException(nameof(statusCode), (int) statusCode, typeof(HttpStatusCode));
+            if (routeName == null) throw new ArgumentNullException(nameof(routeName));
+            if (routeValues == null) throw new ArgumentNullException(nameof(routeValues));
+            if (urlFactory == null) throw new ArgumentNullException(nameof(urlFactory));
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            StatusCode = statusCode;
+            RouteName = routeName;
+            RouteValues = routeValues;
+            _urlFactory = urlFactory;
+            _request = request;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute());
+        }
+
+        private HttpResponseMessage Execute()
+        {
+            var httpResponseMessage = new HttpResponseMessage(StatusCode);
+
+            try
+            {
+                var uriString = _urlFactory.Link(RouteName, RouteValues);
+
+                if (uriString == null)
+                {
+                    throw new InvalidOperationException("Link must not return null.");
+                }
+
+                httpResponseMessage.Headers.Location = new Uri(uriString);
+                httpResponseMessage.RequestMessage = _request;
+            }
+            catch
+            {
+                httpResponseMessage.Dispose();
+                throw;
+            }
+
+            return httpResponseMessage;
+        }
+    }
+}
