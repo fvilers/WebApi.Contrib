a544e5f434c2c4f36c2b7b9113a4c25aaac01d6c
Fabian Vilers <fabian.vilers@dev-one.com>
2017-02-02 10:02:32 +0100

    Add a base location result

---
+++ src/WebApi.Contrib.Tests/Results/LocationResultTests.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Routing;
+using WebApi.Contrib.Results;
+
+namespace WebApi.Contrib.Tests.Results
+{
+    [TestClass]
+    public class LocationResultTests
+    {
+        private const string DefaultLocation = "http://localhost/";
+
+        [TestMethod]
+        public async Task ExecuteAsyncShouldReturnAResponseWithTheSpecifiedStatusCode()
+        {
+            // Arrange
+            const HttpStatusCode statusCode = HttpStatusCode.TemporaryRedirect;
+            var sut = MakeSut(statusCode);
+
+            // Act
+            var result = await sut.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(statusCode, result.StatusCode);
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsyncShouldReturnAResponseWithTheSpecifiedRequest()
+        {
+            // Arrange
+            var request = MakeRequest();
+            var sut = MakeSut(request: request);
+
+            // Act
+            var result = await sut.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(request, result.RequestMessage);
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsyncShouldReturnAResponseWithTheSpecifiedControllerRequest()
+        {
+            // Arrange
+            var request = MakeRequest();
+            var sut = MakeSutWithController(controller: MakeController(request));
+
+            // Act
+            var result = await sut.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(request, result.RequestMessage);
+        }
+
+        [ExpectedException(typeof(UriFormatException))]
+        [TestMethod]
+        public async Task ExecuteAsyncShouldThrowAnExceptionForInvalidLocationFormat()
+        {
+            // Arrange
+            var sut = MakeSut(location: "invalid format");
+
+            // Act
+            await sut.ExecuteAsync(CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsyncShouldReturnAResponseWithTheSpecifiedLocation()
+        {
+            // Arrange
+            var location = new Uri("http://localhost/somewhere");
+            var sut = MakeSut(location: location.ToString());
+
+            // Act
+            var result = await sut.ExecuteAsync(CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(location, result.Headers.Location);
+        }
+
+        private static HttpRequestMessage MakeRequest()
+        {
+            var request = new HttpRequestMessage();
+
+            return request;
+        }
+
+        private static ApiController MakeController(HttpRequestMessage request = null)
+        {
+            var controller = new TestController
+            {
+                ControllerContext = new HttpControllerContext
+                (
+                    new HttpConfiguration(),
+                    new HttpRouteData(new HttpRoute()),
+                    request ?? MakeRequest()
+                )
+            };
+
+            return controller;
+        }
+
+        private static IHttpActionResult MakeSutWithController(HttpStatusCode? statusCode = null, string location = null, ApiController controller = null)
+        {
+            var sut = new TestResult(
+                statusCode ?? HttpStatusCode.Ambiguous,
+                location ?? DefaultLocation,
+                controller ?? MakeController()
+            );
+
+            return sut;
+        }
+
+        private static IHttpActionResult MakeSut(HttpStatusCode? statusCode = null, string location = null, HttpRequestMessage request = null)
+        {
+            var sut = new TestResult(
+                statusCode ?? HttpStatusCode.Ambiguous,
+                location ?? DefaultLocation,
+                request ?? MakeRequest()
+            );
+
+            return sut;
+        }
+
+        private class TestController : ApiController
+        {
+        }
+
+        private class TestResult : LocationResult
+        {
+            public TestResult(HttpStatusCode statusCode, string location, ApiController controller)
+                : base(statusCode, location, controller)
+            {
+            }
+
+            public TestResult(HttpStatusCode statusCode, string location, HttpRequestMessage request)
+                : base(statusCode, location, request)
+            {
+            }
+        }
+    }
+}
---
+++ src/WebApi.Contrib/Results/LocationResult.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApi.Contrib.Results
+{
+    public abstract class LocationResult : IHttpActionResult
+    {
+        public HttpStatusCode StatusCode { get; }
+        public string Location { get; }
+
+        private readonly HttpRequestMessage _request;
+
+        protected LocationResult(HttpStatusCode statusCode, string location, ApiController controller)
+            : this(statusCode, location, controller.Request)
+        {
+        }
+
+        protected LocationResult(HttpStatusCode statusCode, string location, HttpRequestMessage request)
+        {
+            // Not guarding against not defined enum value as some code are missing, for example 308 (Permanent Redirect)
+            if (location == null) throw new ArgumentNullException(nameof(location));
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            StatusCode = statusCode;
+            Location = location;
+            _request = request;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute());
+        }
+
+        private HttpResponseMessage Execute()
+        {
+            var httpResponseMessage = new HttpResponseMessage(StatusCode);
+
+            try
+            {
+                httpResponseMessage.Headers.Location = new Uri(Location);
+                httpResponseMessage.RequestMessage = _request;
+            }
+            catch
+            {
+                httpResponseMessage.Dispose();
+                throw;
+            }
+
+            return httpResponseMessage;
+        }
+    }
+}
