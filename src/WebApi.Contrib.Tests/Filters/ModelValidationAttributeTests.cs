40c3a13bd62b4e6c271697f0d3d7c162d46a0f44
Fabian Vilers <fabian.vilers@dev-one.com>
2017-02-02 14:38:50 +0100

    Add a model validation attribute

---
+++ src/WebApi.Contrib.Tests/Filters/ModelValidationAttributeTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using WebApi.Contrib.Filters;
+
+namespace WebApi.Contrib.Tests.Filters
+{
+    [TestClass]
+    public class ModelValidationAttributeTests
+    {
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void OnActionExecutingShouldThrowAnExceptionForNullActionContext()
+        {
+            // Arrange
+            var sut = MakeSut();
+
+            // Act
+            sut.OnActionExecuting(null);
+        }
+
+        [TestMethod]
+        public void OnActionExecutingShouldCreateAnErrorResponseIfArguementsContainsNullValue()
+        {
+            // Arrange
+            var actionContext = MakeActionContext();
+            actionContext.ActionArguments.Add("key", null);
+            var sut = MakeSut();
+
+            // Act
+            sut.OnActionExecuting(actionContext);
+
+            // Assert
+            Assert.IsNotNull(actionContext.Response);
+            Assert.AreEqual(HttpStatusCode.BadRequest, actionContext.Response.StatusCode);
+        }
+
+        [TestMethod]
+        public void OnActionExecutingShouldCreateAnErrorResponseIfModelStateIsInvalid()
+        {
+            // Arrange
+            var actionContext = MakeActionContext();
+            actionContext.ModelState.AddModelError("key", "errorMessage");
+            var sut = MakeSut();
+
+            // Act
+            sut.OnActionExecuting(actionContext);
+
+            // Assert
+            Assert.IsNotNull(actionContext.Response);
+            Assert.AreEqual(HttpStatusCode.BadRequest, actionContext.Response.StatusCode);
+        }
+
+        private static HttpActionContext MakeActionContext()
+        {
+            var controllerContext = new HttpControllerContext
+            {
+                Request = new HttpRequestMessage()
+            };
+            var actionContext = new HttpActionContext(controllerContext, new ReflectedHttpActionDescriptor());
+
+            return actionContext;
+        }
+
+        private static ModelValidationAttribute MakeSut()
+        {
+            var sut = new ModelValidationAttribute();
+
+            return sut;
+        }
+    }
+}
---
+++ src/WebApi.Contrib/Filters/ModelValidationAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebApi.Contrib.Filters
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ModelValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (actionContext == null) throw new ArgumentNullException(nameof(actionContext));
+
+            if (actionContext.ActionArguments.ContainsValue(null))
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, "The request is invalid.");
+            }
+            else if (!actionContext.ModelState.IsValid)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, actionContext.ModelState);
+            }
+        }
+    }
+}
