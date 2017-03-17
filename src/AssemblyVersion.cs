f953573974afb3a90445f9a5adad04af56d610f3
Fabian Vilers <fabian.vilers@dev-one.com>
2017-03-17 10:57:45 +0100

    Removed alpha

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -11,4 +11,4 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyInformationalVersion("1.1.0-alpha")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
